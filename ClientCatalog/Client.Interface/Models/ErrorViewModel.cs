19122b4bec4c35f49c3689055f8ca6ed5611eba8
Your Name <you@example.com>
2024-07-25 11:06:09 +0200

    suppression du .vs , front et back fonctionnel : frontt avec header

---
+++ ClientCatalog/Client.Interface/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Client.Interface.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
